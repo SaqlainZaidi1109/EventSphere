504728f28fd6068f00ab28f4b29e7d488b8852e8
SaqlainZaidi1109 <saqlainzaidi1109@gmail.com>
2024-05-29 10:40:45 -0700

    SignIn Api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,31 +2,57 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 //next so we can use the middleware
-export const signupOrganizer = async (req, res, next)=>{
-console.log(req.body) //this to check in terminal
-//saving data in database
-const {username, email, password} = req.body;
-if(!username || !email || !password || username==='' || email==='' || password===''){
-    next(errorHandler(400,'All fields are required.'))
-}
+export const signupOrganizer = async (req, res, next) => {
+    console.log(req.body) //this to check in terminal
+    //saving data in database
+    const { username, email, password } = req.body;
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required.'))
+    }
 
-const hashedPassword = bcryptjs.hashSync(password, 10);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
-const newUser = new User({
-    username,
-    email,
-    password: hashedPassword
-})
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    })
 
-try {
-    
-    await newUser.save();
-    res.json('Organizer Sign Up successful')
-} catch (error) {
-    next(error)
-}
+    try {
+
+        await newUser.save();
+        res.json('Organizer Sign Up successful')
+    } catch (error) {
+        next(error)
+    }
 }
 
 //we will use async since we are signing up the user it takes time, then we will send the response to the user
+
+//Sign In Organizer
+export const signinOrganizer = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required.'))
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found.'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid password.'))
+        }
+        
+        const token = jwt.sign( {id: validUser._id}, process.env.JWT_SECRET )
+        res.status(200).cookie('access_token',token,{httpOnly:true}).json(validUser);
+    } catch (error) {
+
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signupOrganizer } from "../controllers/auth.controller.js";
+import { signinOrganizer, signupOrganizer } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 //since we need to create something this is going to be a post request
 router.post('/signupOrganizer',signupOrganizer)
+router.post('/signinOrganizer',signinOrganizer)
 
 export default router;
